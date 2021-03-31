0398b3a0964377be04d5006796251a20e76bb859
Karlton Shoemaker <karltonshoemaker@gmail.com>
2021-03-31 16:54:11 -0400

    Successfully retrieved a specified data item from the API via fetch method.

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
-const test = document.getElementById("test");
+let test = document.getElementById("test");
 test.style.color = "brown";
+
+fetch("https://lingua-robot.p.rapidapi.com/language/v1/entries/en/squelch", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "5528b23ad4msha4ad47087d0d6e0p1bd980jsn5eefe831977c",
+		"x-rapidapi-host": "lingua-robot.p.rapidapi.com"
+	}
+})
+.then(response => response.json())
+.then(data => {
+    //JSON.parse(response);
+	console.log(data);
+    console.log(data.entries[0].entry);
+    test.innerText = data.entries[0].entry;
+})
+.catch(err => {
+	console.error(err);
+});
