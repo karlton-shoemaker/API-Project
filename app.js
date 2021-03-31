ae8bdfe0f299a6f1bcce6ca785965e7040a51bca
Karlton Shoemaker <karltonshoemaker@gmail.com>
2021-03-31 17:25:51 -0400

    Added variables to capture different characteristics of objects that were desirable.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let test = document.getElementById("test");
 test.style.color = "brown";
 
-fetch("https://lingua-robot.p.rapidapi.com/language/v1/entries/en/squelch", {
+fetch("https://lingua-robot.p.rapidapi.com/language/v1/entries/en/link", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "5528b23ad4msha4ad47087d0d6e0p1bd980jsn5eefe831977c",
@@ -10,10 +10,17 @@ fetch("https://lingua-robot.p.rapidapi.com/language/v1/entries/en/squelch", {
 })
 .then(response => response.json())
 .then(data => {
-    //JSON.parse(response);
 	console.log(data);
-    console.log(data.entries[0].entry);
-    test.innerText = data.entries[0].entry;
+    //console.log(data.entries[0].pronunciations[0].transcriptions[0]);
+    let word = data.entries[0].entry;
+    let definition = data.entries[0].lexemes[0].senses[0].definition;
+    let pronunciation = data.entries[0].pronunciations[0].audio.url;
+    let partOfSpeech = data.entries[0].lexemes[0].partOfSpeech;
+    let profanityCheck = data.entries[0].lexemes[0].senses[0].labels;
+    let transcription = data.entries[0].pronunciations[0].transcriptions[0].transcription;
+    let alphabet = data.entries[0].pronunciations[0].transcriptions[0].notation;
+    let wordCharacteristics = [word, definition, pronunciation, partOfSpeech, profanityCheck, transcription, alphabet];
+    test.innerText = wordCharacteristics;
 })
 .catch(err => {
 	console.error(err);
